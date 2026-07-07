5a363d186683d2f0f079c5729903a86008c6456b
Jack Brubaker <jbrubaker37@yahoo.com>
2026-07-07 17:10:10 +0000

    remove commented out portion

--- models/intermediate/adtech/int_ad_performance.sql
+++ models/intermediate/adtech/int_ad_performance.sql
@@ -6,13 +6,6 @@ with events as (
 select
     ad_id,
     count(*) as total_events,
-    -- sum(case when event_type = 'Impression' then 1 else 0 end) as impressions,
-    -- sum(case when event_type = 'Click' then 1 else 0 end) as clicks,
-    -- sum(case when event_type = 'Like' then 1 else 0 end) as likes,
-    -- sum(case when event_type = 'Share' then 1 else 0 end) as shares,
-    -- sum(case when event_type = 'Comment' then 1 else 0 end) as comments,
-    -- sum(case when event_type = 'Purchase' then 1 else 0 end) as purchases,
-
     {%- set event_types = ['Impression','Click','Like','Share','Comment','Purchase'] -%}
 
     {% for event_type in event_types %}
